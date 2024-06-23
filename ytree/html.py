c2ea95d31c0c390c167abdd22903610b7761cf8b
Joachim Wiberg <troglobit@gmail.com>
2024-06-23 18:29:35 +0200

    Simplify, move to jstree node types
    Most notably, use different cdn for bootstrap.min.css so we can see the icons.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- ytree/html.py
+++ ytree/html.py
@@ -1,24 +1,33 @@
 import logging
-import libyang
+
 
 def generate_tree(node, depth=0):
-    description = node.description() if node.description() else "No description available"
-    tree = f'<li data-jstree=\'{{"opened": false}}\'><abbr title="{description}">{node.name()}</abbr>'
-    logging.debug("%s - %s - %s", node.name(), node.keyword(), node.nodetype())
+    if node.description():
+        description = node.description()
+    else:
+        description = "No description available"
 
-    if node.keyword() in ['container', 'list', 'choice', 'case']:
+    is_leaf = not hasattr(node, 'children') or not any(node.children())
+    node_type = 'file' if is_leaf else 'default'
+
+    tree = f'<li data-jstree=\'{{"type": "{node_type}"}}\'>' \
+           f'<abbr title="{description}">{node.name()}</abbr>'
+    logging.debug('%s - %s - %s', node.name(), node.keyword(), node.nodetype())
+
+    if not is_leaf:
         tree += '<ul>'
         for child in node.children():
             tree += generate_tree(child, depth + 1)
         tree += '</ul>'
     tree += '</li>'
+
     return tree
 
 
 def generate_html_tree(modules):
     tree_html = '<ul>'
     for module in modules:
-        logging.info("Processing module %s", module.name())
+        logging.info('Processing module %s', module.name())
         for node in module.children():
             tree_html += generate_tree(node)
     tree_html += '</ul>'
@@ -31,24 +40,18 @@ def create_html_output(tree_html, output_file):
 <html>
 <head>
     <title>YANG Tree View</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.12/themes/default/style.min.css" />
     <style>
         body {{
             font-family: Arial, sans-serif;
             margin: 20px;
         }}
-        #jstree {{
-            margin-top: 20px;
-        }}
-        .jstree-icon {{
-            display: none;
-        }}
     </style>
 </head>
 <body>
-    <h1>YANG Tree View</h1>
-    <button id="expand-all">Expand All</button>
-    <button id="collapse-all">Collapse All</button>
+    <h1>YANG Tree View <input type="text" id="search-box" placeholder="Search..."></h1>
+    <button id="toggle-all"><i class="glyphicon glyphicon-plus-sign"></i></button>
     <div id="jstree">
         {tree_html}
     </div>
@@ -60,26 +63,43 @@ def create_html_output(tree_html, output_file):
             $('#jstree').jstree({{
                 "core": {{
                     "themes": {{
+                        "dots": false,
+                        "responsive": false,
+                        "stripes": true,
                         "variant": "large"
+                    }},
+                    "check_callback": true
+                }},
+                "types": {{
+                    "default": {{
+                        "icon": "glyphicon glyphicon-folder-close"
+                    }},
+                    "file": {{
+                        "icon": "glyphicon glyphicon-leaf"
                     }}
                 }},
-                "plugins": ["search"]
+                "plugins": ["search", "types"]
             }});
 
-            $('#expand-all').click(function() {{
-                $('#jstree').jstree('open_all');
+            var isExpanded = false;
+            $('#toggle-all').click(function() {{
+                if (isExpanded) {{
+                    $('#jstree').jstree('close_all');
+                    $(this).find('i').removeClass('glyphicon-minus-sign').addClass('glyphicon-plus-sign');
+                }} else {{
+                    $('#jstree').jstree('open_all');
+                    $(this).find('i').removeClass('glyphicon-plus-sign').addClass('glyphicon-minus-sign');
+                }}
+                isExpanded = !isExpanded;
             }});
 
-            $('#collapse-all').click(function() {{
-                $('#jstree').jstree('close_all');
-            }});
-
-            $('#jstree').on('open_node.jstree close_node.jstree', function (e, data) {{
-                data.instance.set_icon(data.node, data.node.state.opened ? 'glyphicon glyphicon-minus' : 'glyphicon glyphicon-plus');
-            }});
-
-            $('#jstree').jstree(true).get_json('#', {{ 'flat': true }}).forEach(function (node) {{
-                $('#jstree').jstree('set_icon', node.id, 'glyphicon glyphicon-plus');
+            var to = false;
+            $('#search-box').keyup(function () {{
+                if(to) {{ clearTimeout(to); }}
+                to = setTimeout(function () {{
+                    var v = $('#search-box').val();
+                    $('#jstree').jstree(true).search(v);
+                }}, 250);
             }});
         }});
     </script>
@@ -87,7 +107,7 @@ def create_html_output(tree_html, output_file):
 </html>
 """
 
-    with open(output_file, 'w', encoding="utf-8") as file:
+    with open(output_file, 'w', encoding='utf-8') as file:
         file.write(html_template)
 
-    logging.info("HTML file generated: %s", output_file)
+    logging.info('HTML file generated: %s', output_file)
